e65d040aa3396ac8a83f937d9c304d7a7ac2ba5f
Bharat Kumar Gupta <bharatkgupta2000@gmail.com>
2024-07-18 23:08:03 +0530

    Time: 88 ms (53.70%), Space: 57.3 MB (54.79%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,33 +1,41 @@
 class Solution {
 public:
-    bool isPalindrome(string s, int start, int end) {
-        while(start<end) {
-            if(s[start] != s[end]) {
-                return false;
-            }
-            start++;
-            end--;
-        }
-        return true;
-    }
-    void backtrackPartition(string s, int index, vector<string>& temp, vector<vector<string>>& ans) {
+    void backtrackPartition(string s, int index, vector<string>& temp, vector<vector<string>>& ans, vector<vector<bool>>& isPalindrome) {
         int len = s.length();
         if(index == len) {
             ans.push_back(temp);
             return;
         }
         for(int i=index;i<len;i++) {
-            if(isPalindrome(s, index, i)) {
+            if(isPalindrome[index][i]) {
                 temp.push_back(s.substr(index, i-index+1));
-                backtrackPartition(s, i+1, temp, ans);
+                backtrackPartition(s, i+1, temp, ans, isPalindrome);
                 temp.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
+        int n = s.length();
+        vector<vector<bool>> isPalindrome(n, vector<bool> (n, false));
+        for(int i=0;i<n;i++) {
+            int l = i, r = i;
+            while(s[r+1] == s[r]) {
+                r++;
+            }
+            for(int j=l;j<=r;j++) {
+                for(int k=j;k<=r;k++) {
+                    isPalindrome[j][k] = true;
+                }
+            }
+            while(l>=0 && r<n && s[l] == s[r]) {
+                isPalindrome[l][r] = true;
+                l--;
+                r++;
+            }
+        }
         vector<vector<string>> ans;
         vector<string> temp;
-        backtrackPartition(s, 0, temp, ans);
+        backtrackPartition(s, 0, temp, ans, isPalindrome);
         return ans;
     }
 };
