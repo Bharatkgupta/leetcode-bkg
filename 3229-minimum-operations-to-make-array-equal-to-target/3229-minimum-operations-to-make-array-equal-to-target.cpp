28b485d033d5178f693ac681bfd1ab547fbbdfad
Bharat Kumar Gupta <bharatkgupta2000@gmail.com>
2024-07-22 21:06:49 +0530

    Time: 129 ms (100.00%), Space: 122.2 MB (100.00%) - LeetHub

---
+++ 3229-minimum-operations-to-make-array-equal-to-target/3229-minimum-operations-to-make-array-equal-to-target.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long minimumOperations(vector<int>& nums, vector<int>& target) {
+        int n = nums.size();
+        long long incr = 0, decr = 0, ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int diff = target[i] - nums[i];
+
+            if (diff > 0) {
+                if (incr < diff)
+                    ans += diff - incr;
+                incr = diff;
+                decr = 0;
+            } else if (diff < 0) {
+                if (diff < decr)
+                    ans += decr - diff;
+                decr = diff;
+                incr = 0;
+            } else {
+                incr = decr = 0;
+            }
+        }
+
+        return ans;
+    }
+};
