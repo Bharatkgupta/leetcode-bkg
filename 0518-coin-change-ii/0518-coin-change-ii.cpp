a8e5da95c7a3c9108dd3848f11a00f6f7b9f05ba
Bharat Kumar Gupta <bharatkgupta2000@gmail.com>
2024-07-30 23:39:15 +0530

    Time: 5 ms (82.28%), Space: 8.8 MB (86.05%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -4,8 +4,8 @@ public:
         vector<int> dp(amount+1, 0);
         dp[0] = 1;
         for(const int& coin : coins) {
-            for(int i=1;i<=amount;i++) {
-                dp[i] = dp[i] + (i>=coin ? dp[i-coin] : 0);
+            for(int i=coin;i<=amount;i++) {
+                dp[i] += dp[i-coin];
             }
         }
         return dp[amount];
