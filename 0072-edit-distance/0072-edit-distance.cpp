e2cdc7d31b7bc6d616f33f87203bd1c01d2ba4fe
Bharat Kumar Gupta <bharatkgupta2000@gmail.com>
2024-08-08 21:37:32 +0530

    Time: 0 ms (100.00%), Space: 10.3 MB (86.81%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,24 +2,23 @@ class Solution {
 public:
     int minDistance(string word1, string word2) {
         int l1 = word1.length(), l2 = word2.length();
-        vector<vector<int>> dp(l1+1, vector<int> (l2+1, 0));
-        for(int i=0;i<=l1;i++) {
-            dp[i][0] = i;
-        }
+        vector<int> prev(l2+1,0), curr(l2+1,0);
         for(int i=0;i<=l2;i++) {
-            dp[0][i] = i;
+            prev[i] = i;
         }
         for(int i1=1;i1<=l1;i1++) {
+            curr[0] = i1;
             for(int i2=1;i2<=l2;i2++) {
                 if(word1[i1-1] == word2[i2-1]) {
-                    dp[i1][i2] = dp[i1-1][i2-1];
+                    curr[i2] = prev[i2-1];
                 } else {
-                    dp[i1][i2] = 1+min(dp[i1][i2-1], // insert
-                                       min(dp[i1-1][i2], // delete
-                                          dp[i1-1][i2-1])); // replace
+                    curr[i2] = 1+min(curr[i2-1], // insert
+                                       min(prev[i2], // delete
+                                          prev[i2-1])); // replace
                 }
             }
+            prev = curr;
         }
-        return dp[l1][l2];
+        return prev[l2];
     }
 };
