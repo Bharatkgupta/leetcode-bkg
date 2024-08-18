d7c5436a1329f9932ad16f1de0f8c46d473edc03
Bharat Kumar Gupta <bharatkgupta2000@gmail.com>
2024-08-18 12:28:39 +0530

    Time: 2 ms (62.59%), Space: 7.4 MB (32.17%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int numTrees(int n) {
-        vector<int> dp(n+1);
-        dp[0] = dp[1] = 1;
-        for(int i=2;i<=n;i++) {
-            for(int j=0;j<i;j++) {
-                dp[i] += dp[j] * dp[i-j-1];
-            }
+        long ans = 1;
+        for(int i=0;i<n;i++) {
+            ans *= (2*n-i);
+            ans /= (i+1);
         }
-        return dp[n];
+        return ans/(n+1);
     }
 };
