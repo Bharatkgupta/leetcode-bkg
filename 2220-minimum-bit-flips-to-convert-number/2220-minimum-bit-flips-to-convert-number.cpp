65064c2f5d009d360c8d481dc83a493fc1867a3d
Bharat Kumar Gupta <bharatkgupta2000@gmail.com>
2024-09-11 21:07:06 +0530

    Time: 1 ms (42.49%), Space: 7.6 MB (44.87%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -3,7 +3,6 @@ public:
     int minBitFlips(int start, int goal) {
         int ans = 0;
         for(int i=0;i<31;i++) {
-            cout<<(1<<i)<<endl;
             if((start & (1<<i)) != (goal & (1<<i))) {
                 ans++;
             }
