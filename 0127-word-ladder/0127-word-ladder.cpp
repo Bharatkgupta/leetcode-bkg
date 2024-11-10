237ad67dfb0d20f25c8b8536afc86290db9d9792
Bharat Kumar Gupta <bharatkgupta2000@gmail.com>
2024-11-10 19:14:44 +0530

    Time: 154 ms (45.37%), Space: 35 MB (16.47%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,90 @@
+class TrieNode {
+public:
+    TrieNode* child[26];
+    bool end;
+};
+
+class Trie {
+    TrieNode* trie;
+public:
+    Trie(vector<string>& words) {
+        trie = new TrieNode();
+        
+        for(const string& word : words) {
+            TrieNode* temp = trie;
+            for(const char& letter : word) {
+                if(temp->child[letter-'a'] == nullptr) {
+                    temp->child[letter-'a'] = new TrieNode();
+                }
+                temp = temp->child[letter-'a'];
+            }
+            temp->end = true;
+        }
+    }
+    
+    bool find(string word) {
+        TrieNode* temp = trie;
+        for(const char& letter : word) {
+            if(temp->child[letter-'a'] == nullptr) {
+                return false;
+            }
+            temp = temp->child[letter-'a'];
+        }
+        if(temp->end == true) {
+            temp->end = false;
+        } else {
+            return false;
+        }
+        return true;
+    }
+};
+
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        Trie trie = Trie(wordList);
+        queue<string> q;
+        q.push(beginWord);
+        int dist = 1, sl = beginWord.size();
+        bool found = false;
+        while(!q.empty()) {
+            int len = q.size();
+            for(int i=0;i<len;i++) {
+                string curr = q.front();
+                q.pop();
+                cout<<curr<<" - ";
+                
+                for(int j=0;j<sl;j++) {
+                    for(char c='a';c<='z';c++) {
+                        char temp = curr[j];
+                        curr[j] = c;
+                        if(temp != c && trie.find(curr)) {
+                            cout<<curr<<" ";
+                            q.push(curr);
+                            if(curr == endWord) {
+                                found=true;
+                                break;
+                            }
+                        }
+                        curr[j] = temp;
+                    }
+                    if(found) {
+                        break;
+                    }
+                }
+                cout<<endl;
+                if(found) {
+                    break;
+                }
+            }
+            dist++;
+            if(found) {
+                break;
+            }
+        }
+        if(!found) {
+            return 0;
+        }
+        return dist;
+    }
+};
