3e766780f38e1bc952da5a2156026912329a16ef
Bharat Kumar Gupta <bharatkgupta2000@gmail.com>
2026-05-25 09:01:59 +0530

    Language: .java, Problem: 0242-valid-anagram, Time: 05-25-2026

---
+++ 0242-valid-anagram/0242-valid-anagram-05-25-2026-09-01-58.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] charFreq = new int[26];
+        
+        for (int i=0; i < s.length(); i++) {
+            charFreq[s.charAt(i)-'a'] = charFreq[s.charAt(i)-'a'] + 1;
+            charFreq[t.charAt(i)-'a'] = charFreq[t.charAt(i)-'a'] - 1;
+        }
+
+        for (int i=0; i < 26; i++) {
+            if (charFreq[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
